f435732b2505c5bb36f2ff731dd4a56bf57cc86d
Mayur Shinde <mayurshinde0315@gmail.com>
2026-08-20 16:30:40 +0530

    Logic Building Using C Programming

---
+++ C_Programming/Competitive/LB22_01.c
@@ -0,0 +1,32 @@
+/* Accept number from user and display below pattern.
+ Input :  5
+ Output : A   B   C   D   E   F
+*/
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    char ch = 'A';
+
+    for(iCnt = 0; iCnt <= iNo; iCnt++)
+    {
+        printf("%c\t",ch);
+        ch++;
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
